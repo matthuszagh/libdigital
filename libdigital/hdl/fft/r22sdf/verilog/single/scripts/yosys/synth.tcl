63fcbf86657b3a9116529d46505b7db813f0d3e5
Matt Huszagh <huszaghmatt@gmail.com>
2019-10-26 16:51:47 -0700

    fft: remove unneeded yosys includes

--- libdigital/hdl/fft/r22sdf/verilog/single/scripts/yosys/synth.tcl
+++ libdigital/hdl/fft/r22sdf/verilog/single/scripts/yosys/synth.tcl
@@ -1,9 +1,7 @@
 yosys -import
 # read_verilog -sv $::env(VERILOG_INCS) $::env(TOP_MODULE_SRC)
 read_verilog -sv -I/home/matt/src/libdigital/libdigital/hdl/memory/shift_reg \
-    -I/home/matt/src/libdigital/libdigital/hdl/memory/ram/old/single_port \
     -I/home/matt/src/libdigital/libdigital/hdl/memory/ram \
-    -I/home/matt/src/libdigital/libdigital/hdl/dsp/multiply_add \
     $::env(TOP_MODULE_SRC)
 hierarchy
 procs;;
