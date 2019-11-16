3676a81081ad699efe378edf77663354ed8ffd01
Matt Huszagh <huszaghmatt@gmail.com>
2019-11-15 18:28:39 -0800

    remove dsp modules
    It is better to use inference for these operations.

--- libdigital/hdl/dsp/multiply_accumulate/vivado_resources.tcl
+++
@@ -1,3 +0,0 @@
-read_verilog -sv mult_accum.v
-synth_design -top mult_accum -part xc7a15tftg256-1
-report_utilization
--- libdigital/hdl/dsp/multiply_add/vivado_resources.tcl
+++
@@ -1,3 +0,0 @@
-read_verilog -sv mult_add.v
-synth_design -top mult_add -part xc7a15tftg256-1
-report_utilization
