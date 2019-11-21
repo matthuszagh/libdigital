a2e90ab04d673ce4f327e97ddfce4c1aa6cde3a6
Matt Huszagh <huszaghmatt@gmail.com>
2019-11-20 18:17:36 -0800

    fir_poly: fix fir frequency bands in tap generation script

--- libdigital/hdl/filters/fir_poly/scripts/gen_taps.py
+++ libdigital/hdl/filters/fir_poly/scripts/gen_taps.py
@@ -4,7 +4,7 @@ from libdigital.tools.fir import FIR
 
 fir = FIR(
     numtaps=120,
-    bands=[0, 0.95e6, 1e6, 20e6],
+    bands=[0, 1e6, 1.5e6, 20e6],
     band_gain=[1, 0],
     fs=40e6,
     pass_db=0.5,
@@ -17,5 +17,6 @@ downsample_factor = 20
 fir.write_poly_taps_files(
     ["../taps"], tap_bits, downsample_factor, True, False
 )
+fir.plot_response("freq_response.png")
 print("normalization shift: {}".format(fir.tap_normalization_shift()))
 print("output bits: {}".format(fir.output_bit_width(input_bits)))
